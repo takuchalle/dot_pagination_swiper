ffe8fee2cbf6299296492f2b3bb3e6310af5941d
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-24 18:26:00 +0900

    Update sample test

--- test/dot_pagination_swiper_test.dart
+++ test/dot_pagination_swiper_test.dart
@@ -5,6 +5,9 @@ import '../lib/src/dot_pagination.dart';
 void main() {
   testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
     // Test code goes here.
-    await tester.pumpWidget(DotPagination());
+    await tester.pumpWidget(DotPagination(
+      itemCount: 10,
+      activeIndex: 0,
+    ));
   });
 }
