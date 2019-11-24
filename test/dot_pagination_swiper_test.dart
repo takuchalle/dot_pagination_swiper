8eeef4e0c8952eb1007d94013b0644b114ee4a32
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-25 08:11:58 +0900

    Update sample test

--- test/dot_pagination_swiper_test.dart
+++ test/dot_pagination_swiper_test.dart
@@ -1,13 +1,42 @@
+import 'package:dot_pagination_swiper/dot_pagination_swiper.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/src/dot_pagination.dart';
+const List<String> kStates = <String>[
+  'Alabama',
+  'Alaska',
+  'Arizona',
+  'Arkansas',
+  'California',
+  'Colorado',
+  'Connecticut',
+];
 
 void main() {
   testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
-    // Test code goes here.
-    await tester.pumpWidget(DotPagination(
-      itemCount: 10,
-      activeIndex: 0,
+    final List<String> log = <String>[];
+
+    await tester.pumpWidget(Directionality(
+      textDirection: TextDirection.ltr,
+      child: DotPaginationSwiper(
+        children: kStates.map<Widget>((String state) {
+          return GestureDetector(
+            onTap: () {
+              log.add(state);
+            },
+            child: Container(
+              height: 200.0,
+              color: const Color(0xFF0000FF),
+              child: Text(state),
+            ),
+          );
+        }).toList(),
+      ),
     ));
+    await tester.tap(find.text('Alabama'));
+    expect(log, equals(<String>['Alabama']));
+    log.clear();
+
+    expect(find.text('Alaska'), findsNothing);
   });
 }
