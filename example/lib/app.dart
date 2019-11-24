650da4f898b93747d3cdca98d0ee90a67284e1a1
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-24 11:12:56 +0900

    [example] Chhange primary color blue

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -9,6 +9,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         body: Center(
           child: Builder(
