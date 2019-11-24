7d431223e78ca4b072d6c469069c3802df6a53aa
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-24 18:21:34 +0900

    Add sample test

--- test/dot_pagination_swiper_test.dart
+++ test/dot_pagination_swiper_test.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/src/dot_pagination.dart';
+
+void main() {
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Test code goes here.
+    await tester.pumpWidget(DotPagination());
+  });
+}
